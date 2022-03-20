f445f2b9ec8f8850f5911acd8dab8fa3b07b984f
Skyler Turner <skyler@clicksminuteper.net>
2022-03-20 23:00:07 +0000

    Add program.cs (not sure why it was excluded from HEAD^1)

---
+++ Match4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Match4
+{ 
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Match4!");
+            Console.WriteLine("Do you want to view the rules? (y/n)");
+            string choice;
+            do
+            {
+                Console.Write("> ");
+                choice = (Console.ReadLine() ?? "").ToLower();
+            } while (choice != "y" && choice != "n");
+
+            if (choice == "y")
+            {
+                Game.DisplayRules();
+            }
+            
+            int playerCount;
+            Console.WriteLine("How many players do you want to play with? (2-6)");
+            do
+            {
+                Console.Write("> ");
+            } while (!(int.TryParse(Console.ReadLine(), out playerCount) && playerCount > 1 && playerCount < 7));
+            
+            var game = new Game(playerCount);
+            game.Start();
+        }
+    }
+}
